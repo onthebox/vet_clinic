12983cebaabf982cae61d239bda9a8c4e9cb0ed7
onthebox <onthebox@yandex.ru>
2023-11-08 00:38:21 +0300

    moved to fastapi 0.103.2, small fixes in app

--- src/main.py
+++ src/main.py
@@ -57,7 +57,7 @@ async def post_timestamp() -> Dict[int, int]:
 
 
 @app.get('/dog', response_model=List[Dog], summary='Get Dogs')
-async def get_dog_by_kind(kind: str | None = None) -> List[Dict[str, Union[str, int, DogType]]]:
+async def get_dog_by_kind(kind: str | None = None) -> List[Dog]:
     """Get the dogs by their kind or get all the dogs from the database"""
     if kind:
         query_result = [doggy for doggy in dogs_db.values() if doggy.kind.value == kind.lower()]
@@ -67,7 +67,7 @@ async def get_dog_by_kind(kind: str | None = None) -> List[Dict[str, Union[str, 
 
 
 @app.post('/dog', response_model=Dog, summary='Post New Dog')
-async def create_new_dog(name: str, pk: int, kind: str) -> Dict[str, Union[str, int, DogType]]:
+async def create_new_dog(name: str, pk: int, kind: str) -> Dog:
     """Post new dog entry to the database"""
     if dogs_db.get(pk, False):
         raise HTTPException(status_code=409, detail="Dog with such primary key already exists.")
@@ -80,16 +80,16 @@ async def create_new_dog(name: str, pk: int, kind: str) -> Dict[str, Union[str, 
     return dogs_db[pk]
 
 
-@app.get('/dog/{pk}', response_model=Dog, summary='Edit Existent Dog')
-async def get_dog_by_pk(pk: int) -> Dict[str, Union[str, int, DogType]]:
+@app.get('/dog/{pk}', response_model=Dog, summary='Get Dog By Primary Key')
+async def get_dog_by_pk(pk: int) -> Dog:
     """Get dog by its primary key"""
     query_dog = dogs_db.get(pk, [])
 
     return query_dog
 
 
-@app.patch('/dog/{pk}')
-async def patch_dog_by_pk(pk: int, name: str, kind: str) -> Dict[str, Union[str, int, DogType]]:
+@app.patch('/dog/{pk}', response_model=Dog, summary='Edit Existent Dog')
+async def patch_dog_by_pk(pk: int, name: str, kind: str) -> Dog:
     """Edit dog entry by primary key"""
     if not dogs_db.get(pk, False):
         raise HTTPException(status_code=409, detail="Dog with such primary key doesn't exist.")
